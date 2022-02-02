d6a21de68fe4af0ddadd75187eeceb0114835abf
Julian Herlein <julian.n.herlein@accenture.com>
2022-02-02 20:10:30 -0300

    first changes in the webpage

---
+++ script.js
@@ -0,0 +1,26 @@
+const quizData = [
+    {
+        question:"How old is Florin?",
+        a: "10",
+        b: "17",
+        c: "26",
+        d: "110",
+        correct: "c"
+    },
+    {
+        question: "What is the best programming language?",
+        a:"Java",
+        b:"C",
+        c:"Python",
+        d:"JavaScript",
+        correct: "a"
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "Donald Trump",
+        c: "Ivan Saldano",
+        d: "Mihai",
+        correct: "b"
+    }
+]
